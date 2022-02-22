48e0616798396c15ef03bec9f427cf230e4e5365
Paul Wang <paul.wang@onedegree.hk>
2022-02-22 12:18:49 +0800

    first commit

---
+++ pages/api/revalidate.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  console.log('req.method', req.method);
+  console.log('req.body', req.body);
+
+  try {
+    await res.unstable_revalidate('/');
+    return res.status(200).send('Success!');
+  } catch (error) {
+    console.log(error);
+    return res.status(401).send('fail!');
+  }
+}
